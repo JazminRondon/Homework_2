a9c97deda7c5f31cef5637d7dd488cb8f1f24c0d
JazminRondon <Jazmin_Rondon@outlook.com>
2022-08-31 12:47:39 -0400

    Initial commit

---
+++ src/main/java/Examples.java
@@ -0,0 +1,38 @@
+public class Examples {
+    public static void main(String[] args) {
+
+
+
+    /* Java HOME
+    Above is an example of how all Java codes should begin.
+    There's a specific was you have to start off for all Coding. After creating a project, a class must be created
+    It has to be public so other classes can call on it.
+    Static is a method to call on the class without needing something to run it agaist.
+    Void is the return type of your main method
+    Main, just because
+    shortcut for public static void main= psvm
+     */
+        //Java Syntax is the rules of how Java is written
+
+        System.out.println("Explaining Java Basics for Homework 2");
+
+    /*Java Output can be done using println() to output values of print text
+        An example of how to output a text is used above
+
+      Java Comments is what I am currently doing now. I'm doing Multi-line comments to be exact.
+
+     */
+     //This is an example of single line comments. The multi was opened with /* and closed with */
+    /* Java Variables and Data Types
+        Variables are used to temporarily store data in computer's memory. In Java, the type of a variable should be
+        specified at the time of declaration. There are Primitives and Reference types.
+        The eight Primatives are byte,short, int, long, float, double, char, and boolean.
+        Reference types don't store actual objects in memory. They store the address of an object in memory.
+        Reference types are more complex
+     */
+        int myAge = 27;
+        System.out.println("Java Variable Example:" + myAge);
+
+    }
+
+}
